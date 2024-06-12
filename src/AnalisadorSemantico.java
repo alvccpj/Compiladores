aa144a8d20894b9f6eaed2e6939fd2bc9da8ea89
ribau <vivinhovivo10@gmail.com>
2024-06-12 19:00:24 -0300

    semantico

--- src/AnalisadorSemantico.java
+++ src/AnalisadorSemantico.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 public class AnalisadorSemantico {
 
-    // Método principal que verifica a tipagem do código fonte fornecido
-    public static boolean verificarTipagem(String codigoFonte) {
+    // Método principal que realiza a verificação de tipos do código fornecido
+    public static boolean validarTipos(String codigo) {
         try {
-            List<String> tokens = Arrays.asList(codigoFonte.split("\\s+"));
-            ListIterator<String> iterador = tokens.listIterator();
+            List<String> elementos = Arrays.asList(codigo.split("\\s+"));
+            ListIterator<String> iterador = elementos.listIterator();
 
             while (iterador.hasNext()) {
-                String token = iterador.next();
+                String elemento = iterador.next();
 
-                if (token.equals("int") || token.equals("float") || token.equals("double") || token.equals("char")
-                        || token.equals("boolean")) {
-                    if (!verificarDeclaracaoVariavelOuFuncao(iterador, token)) {
+                if (isTipo(elemento)) {
+                    if (!validarDeclaracaoVarOuFunc(iterador, elemento)) {
                         return false;
                     }
-                } else if (token.equals("struct")) {
-                    if (!verificarDeclaracaoEstrutura(iterador)) {
+                } else if (elemento.equals("struct")) {
+                    if (!validarDeclaracaoStruct(iterador)) {
                         return false;
                     }
-                } else if (token.equals("//")) {
-                    ignorarComentarioLinha(iterador);
-                } else if (token.equals("/*")) {
-                    ignorarComentarioBloco(iterador);
+                } else if (elemento.equals("//")) {
+                    pularComentarioLinha(iterador);
+                } else if (elemento.equals("/*")) {
+                    pularComentarioBloco(iterador);
                 } else {
-                    if (token.endsWith(";")) {
+                    if (elemento.endsWith(";")) {
                         // Lógica para declaração de variável
                     }
                 }
@@ -38,31 +37,31 @@ public class AnalisadorSemantico {
         }
     }
 
-    // Verifica a declaração de variável ou função
-    private static boolean verificarDeclaracaoVariavelOuFuncao(ListIterator<String> it, String tipo) {
+    // Método para verificar a declaração de variáveis ou funções
+    private static boolean validarDeclaracaoVarOuFunc(ListIterator<String> it, String tipo) {
         if (it.hasNext()) {
-            String id = it.next();
-            if (id.matches("[a-zA-Z_]\\w*")) {
+            String identificador = it.next();
+            if (identificador.matches("[a-zA-Z_]\\w*")) {
                 if (it.hasNext()) {
-                    String nextToken = it.next();
-                    if (nextToken.equals(";")) {
+                    String proximoToken = it.next();
+                    if (proximoToken.equals(";")) {
                         return true;
-                    } else if (nextToken.equals("=")) {
-                        if (!verificarExpressao(it)) {
+                    } else if (proximoToken.equals("=")) {
+                        if (!validarExpressao(it)) {
                             return false;
                         }
                         if (!it.next().equals(";")) {
                             return false;
                         }
                         return true;
-                    } else if (nextToken.equals("(")) {
-                        if (!verificarParametros(it)) {
+                    } else if (proximoToken.equals("(")) {
+                        if (!validarParametros(it)) {
                             return false;
                         }
                         if (!it.next().equals(")")) {
                             return false;
                         }
-                        if (!verificarBloco(it)) {
+                        if (!validarBloco(it)) {
                             return false;
                         }
                         return true;
@@ -73,8 +72,8 @@ public class AnalisadorSemantico {
         return false;
     }
 
-    // Verifica a expressão para assegurar que a tipagem está correta
-    private static boolean verificarExpressao(ListIterator<String> it) {
+    // Método para validar expressões
+    private static boolean validarExpressao(ListIterator<String> it) {
         Stack<String> pilha = new Stack<>();
 
         while (it.hasNext()) {
@@ -84,46 +83,44 @@ public class AnalisadorSemantico {
                 if (pilha.size() < 2) {
                     return false;
                 }
-                String tipoOperando2 = pilha.pop();
-                String tipoOperando1 = pilha.pop();
-                if (!tiposSaoCompativeis(tipoOperando1, tipoOperando2, token)) {
+                String operando2 = pilha.pop();
+                String operando1 = pilha.pop();
+                if (!tiposCompativeis(operando1, operando2, token)) {
                     return false;
                 }
-                String resultado = inferirTipoResultado(tipoOperando1, tipoOperando2, token);
+                String resultado = inferirTipoResultado(operando1, operando2, token);
                 if (resultado == null) {
                     return false;
                 }
                 pilha.push(resultado);
             } else {
-                pilha.push(inferirTipo(token));
+                pilha.push(determinarTipo(token));
             }
         }
 
         return pilha.size() == 1;
     }
 
-    // Verifica se os tipos são compatíveis para uma operação
-    private static boolean tiposSaoCompativeis(String tipo1, String tipo2, String operador) {
-        if (operador.equals("+") || operador.equals("-") || operador.equals("*") || operador.equals("/")
-                || operador.equals("%")) {
+    // Verifica se os tipos são compatíveis
+    private static boolean tiposCompativeis(String tipo1, String tipo2, String operador) {
+        if (operador.matches("[+\\-*/%]")) {
             return tipo1.equals("int") && tipo2.equals("int");
         } else {
             return false;
         }
     }
 
-    // Infere o tipo do resultado de uma operação
+    // Inferir o tipo de resultado de uma operação
     private static String inferirTipoResultado(String tipo1, String tipo2, String operador) {
-        if (operador.equals("+") || operador.equals("-") || operador.equals("*") || operador.equals("/")
-                || operador.equals("%")) {
+        if (operador.matches("[+\\-*/%]")) {
             return "int";
         } else {
             return null;
         }
     }
 
-    // Infere o tipo de um token
-    private static String inferirTipo(String token) {
+    // Determina o tipo de um token
+    private static String determinarTipo(String token) {
         if (token.matches("\\d+")) {
             return "int";
         } else if (token.matches("\\d+\\.\\d+")) {
@@ -141,10 +138,10 @@ public class AnalisadorSemantico {
     }
 
     // Verifica a lista de parâmetros de uma função
-    private static boolean verificarParametros(ListIterator<String> it) {
+    private static boolean validarParametros(ListIterator<String> it) {
         while (it.hasNext()) {
             String tipoParametro = it.next();
-            if (!isTipoBasico(tipoParametro)) {
+            if (!isTipo(tipoParametro)) {
                 return false;
             }
             String idParametro = it.next();
@@ -164,32 +161,31 @@ public class AnalisadorSemantico {
         return false;
     }
 
-    // Verifica se um tipo é básico (int, float, double, char, boolean)
-    private static boolean isTipoBasico(String tipo) {
+    // Verifica se um tipo é válido
+    private static boolean isTipo(String tipo) {
         return tipo.matches("(int|float|double|char|boolean)");
     }
 
-    // Verifica o bloco de código para assegurar que está bem tipado
-    private static boolean verificarBloco(ListIterator<String> it) {
+    // Verifica se o bloco de código é válido
+    private static boolean validarBloco(ListIterator<String> it) {
         boolean bemTipado = true;
-        int contChaves = 0;
+        int contadorChaves = 0;
 
         while (it.hasNext()) {
             String token = it.next();
 
             if (token.equals("{")) {
-                contChaves++;
+                contadorChaves++;
             } else if (token.equals("}")) {
-                contChaves--;
+                contadorChaves--;
 
-                if (contChaves < 0) {
+                if (contadorChaves < 0) {
                     bemTipado = false;
                     break;
                 }
             }
-
         }
-        if (contChaves != 0) {
+        if (contadorChaves != 0) {
             bemTipado = false;
         }
 
@@ -197,7 +193,7 @@ public class AnalisadorSemantico {
     }
 
     // Ignora comentários de linha
-    private static void ignorarComentarioLinha(ListIterator<String> it) {
+    private static void pularComentarioLinha(ListIterator<String> it) {
         while (it.hasNext()) {
             String token = it.next();
             if (token.equals("\n")) {
@@ -207,7 +203,7 @@ public class AnalisadorSemantico {
     }
 
     // Ignora comentários de bloco
-    private static void ignorarComentarioBloco(ListIterator<String> it) {
+    private static void pularComentarioBloco(ListIterator<String> it) {
         while (it.hasNext()) {
             String token = it.next();
             if (token.equals("*/")) {
@@ -217,12 +213,12 @@ public class AnalisadorSemantico {
     }
 
     // Verifica a declaração de uma estrutura
-    private static boolean verificarDeclaracaoEstrutura(ListIterator<String> it) {
+    private static boolean validarDeclaracaoStruct(ListIterator<String> it) {
         if (it.hasNext()) {
-            String nomeEstrutura = it.next();
-            if (nomeEstrutura.matches("[a-zA-Z_]\\w*")) {
+            String nomeStruct = it.next();
+            if (nomeStruct.matches("[a-zA-Z_]\\w*")) {
                 if (it.hasNext() && it.next().equals("{")) {
-                    if (verificarCamposEstrutura(it)) {
+                    if (validarCamposStruct(it)) {
                         return it.hasNext() && it.next().equals("}");
                     }
                 }
@@ -232,10 +228,10 @@ public class AnalisadorSemantico {
     }
 
     // Verifica os campos dentro de uma estrutura
-    private static boolean verificarCamposEstrutura(ListIterator<String> it) {
+    private static boolean validarCamposStruct(ListIterator<String> it) {
         while (it.hasNext()) {
             String token = it.next();
-            if (isTipoBasico(token)) {
+            if (isTipo(token)) {
                 String nomeCampo = it.next();
                 if (nomeCampo.matches("[a-zA-Z_]\\w*")) {
                     if (it.hasNext() && it.next().equals(";")) {
